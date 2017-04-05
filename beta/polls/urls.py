e45681e98fb2c22d1fedaad8a489c61bdc1a0187
Hari-28 <thkrishnab4u@gmail.com>
2017-04-05 10:12:51 +0530

    Add files via upload

--- beta/polls/urls.py
+++ beta/polls/urls.py
@@ -4,18 +4,30 @@ from .import views
 urlpatterns = [
     #/polls/
     url(r'^$', views.index, name='index'),
+    url(r'^#', views.login, name='login'),
 
-    #/polls/71/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='index'),
-    url(r'^registar.html/$', views.reg, name='reg'),
-    url(r'^correspondent.html/$', views.my_view, name='my_view'),
+
+
     url(r'^correspondent.html/$', views.cor, name='cor'),
-    url(r'^correspondent/#(?P<side_id>[0-9]+)/$', views.side, name='side'),
+    url(r'^c_addstudent.html/$', views.caddstud, name='caddstud'),
+    url(r'^c_addfaculty.html/$', views.caddfac, name='caddfac'),
+    url(r'^c_addregistar.html/$', views.caddreg, name='caddreg'),
+    url(r'^c_addstaff.html/$', views.caddstaff, name='caddstaff'),
+    url(r'^c_request.html/$', views.creq, name='creq'),
+    url(r'^c_approval.html/$', views.capp, name='capp'),
+
+
+
     url(r'^headregistar.html/$', views.hreg, name='hreg'),
-    url(r'^correspondent.html/c_addstudent.html/$', views.caddstud, name='caddstud'),
-    url(r'^correspondent.html/c_addfaculty.html/$', views.caddfac, name='caddfac'),
-    url(r'^correspondent.html/c_addregistar.html/$', views.caddreg, name='caddreg'),
-    url(r'^correspondent.html/c_addstaff.html/$', views.caddstaff, name='caddstaff'),
-    url(r'^correspondent.html/c_request.html/$', views.creq, name='creq'),
-    url(r'^correspondent.html/c_approval.html/$', views.capp, name='capp'),
+    url(r'^h_addstudent.html/$', views.haddstud, name='haddstud'),
+    url(r'^h_addfaculty.html/$', views.haddfac, name='haddfac'),
+    url(r'^h_addstaff.html/$', views.haddstaff, name='haddstaff'),
+    url(r'^h_request.html/$', views.hreq, name='hreq'),
+
+
+
+
+
+
+    url(r'^registar.html/$', views.reg, name='reg'),
 ]
--- beta/polls/views.py
+++ beta/polls/views.py
@@ -8,37 +8,215 @@ def index(request):
     context = {'all_albums':all_albums,}
     return render(request,'polls/login.html',context)
 
-def my_view(request):
+def login(request):
     if request.method == 'POST':
         username = request.POST.get('username11')
         password = request.POST.get('password11')
-        ss="iiitg"
+        ss = "iiitg"
+        ss1 = "iiitghr"
+        ss2  = "iiitgr"
         if username == ss:
-            context346 = {"username": password}
+            context346 = {"username": username}
             return render(request, 'polls/correspondent.html', context346)
+        elif username == ss1:
+            return render(request, 'polls/headregistar.html', )
+        elif username == ss2:
+            return render(request, 'polls/registar.html', )
         else:
-            return HttpResponse("User is not iiitg")
+            return HttpResponse("User is not iiitg" + str(username))
 
-    else:
-        return HttpResponse("Invalid method declared.")
-
-
-def detail(request, album_id):
-    return HttpResponse("<h2>Album_id is " + str(album_id) )
-
-def side(request, side_id):
-    context110 = {}
-    return render(request, 'polls/correspondent.html/'+str(side_id), context110)
 
 
 def cor(request):
-    context0={}
-    return render(request, 'polls/correspondent.html', context0)
+    if request.method == 'POST':
+        div = request.POST.get('div')
+        s1="expenses"
+        s3="salary"
+        s5="fees"
+        s6 = "attend"
+        s7 = "grade"
+        if div == s1:
+            filename = request.POST.get('filename')
+            remark = request.POST.get('remark')
+            #return HttpResponse("User is div : 1    ;" + str(filename) + str(remark))
+            return render(request, 'polls/c_request.html',)
+        elif div == s3:
+            name = request.POST.get('name')
+            id = request.POST.get('id')
+            salary = request.POST.get('salary')
+            amount = request.POST.get('amount')
+            remarks = request.POST.get('remark')
+            #return render(request, 'polls/c_request.html',)
+            return HttpResponse("User is div : 2    ;" + str(name)+ str(id) + str(salary)+ str(amount) + str(remarks))
+        elif div == s5:
+            name = request.POST.get('name')
+            sclass = request.POST.get('class')
+            roll = request.POST.get('roll')
+            amount = request.POST.get('amount')
+            remarks = request.POST.get('remark')
+            return render(request, 'polls/c_request.html',)
+            #return HttpResponse("User is div : 2    ;" + str(name)+ str(sclass) + str(roll)+ str(amount) + str(remarks))
+        elif div == s6:
+            sclass = request.POST.get('class')
+            section = request.POST.get('section')
+            filename = request.POST.get('filename')
+            remark = request.POST.get('remark')
+            return HttpResponse("User is div : 1    ;" + str(sclass) + str(section) + str(filename) + str(remark))
+            #return render(request, 'polls/c_request.html',)
+        elif div == s7:
+            name = request.POST.get('name')
+            sclass = request.POST.get('class')
+            return HttpResponse("User is div : 1    ;" + str(sclass) + str(name) )
+            #return render(request, 'polls/c_request.html',)
+        else:
+            return HttpResponse("Div is not equal to 1 but is equal to " + str(request.POST.get('div')))
+    else:
+        return render(request, 'polls/correspondent.html',)
+
+def caddstud(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        sclass = request.POST.get('class')
+        address = request.POST.get('add')
+        context={}
+        return render(request, 'polls/c_addstudent.html', context)
+    else:
+        return render(request, 'polls/c_addstudent.html',)
+
+def caddfac(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('add')
+        context={}
+        return render(request, 'polls/c_addfaculty.html', context)
+        #return HttpResponse("User is not iiitg" + str(name) + str(email) + str(address))
+    else:
+        return render(request, 'polls/c_addfaculty.html', )
+
+def caddreg(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('add')
+        context={}
+        return render(request, 'polls/c_addregistar.html', context)
+        #return HttpResponse("User is not iiitg" + str(name) + str(email) + str(address))
+    else:
+        return render(request, 'polls/c_addregistar.html',)
+
+
+def caddstaff(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('add')
+        context={}
+        return render(request, 'polls/c_addstaff.html',context )
+        #return HttpResponse("User is not iiitg" + str(name) + str(email) + str(address))
+    else:
+        return render(request, 'polls/c_addstaff.html',)
+
+def creq(request):
+    if request.method == 'POST':
+        div = request.POST.get('div')
+        s1="funds"
+        s2="concession"
+        s3="3"
+        if div == s1:
+            amount = request.POST.get('amount')
+            reason = request.POST.get('reason')
+            #return HttpResponse("User is div : 1    ;" + str(amount) + str(reason))
+            return render(request, 'polls/c_request.html',)
+        elif div == s2:
+            amount = request.POST.get('amount')
+            reason = request.POST.get('reason')
+            name = request.POST.get('name')
+            sclass = request.POST.get('class')
+            return render(request, 'polls/c_request.html',)
+            #return HttpResponse("User is div : 2    ;" + str(amount) + str(reason)+ str(sclass) + str(name))
+        elif div ==s3:
+            return HttpResponse("Div is not equal to 1 but is equal to " + str(request.POST.get('div')))
+    else:
+        return render(request, 'polls/c_request.html',)
+
+    #context45={}
+    #return render(request,'polls/c_request.html',context45)
+
+
+def capp(request):
+    if request.method == 'POST':
+        div = request.POST.get('div')
+        s1="funds"
+        s2="concession"
+        s3="3"
+        if div == s1:
+            amount = request.POST.get('amount')
+            reason = request.POST.get('reason')
+            return HttpResponse("User is div : 1    ;" + str(amount) + str(reason))
+            #return render(request, 'polls/c_approval.html',)
+        elif div == s2:
+            amount = request.POST.get('amount')
+            reason = request.POST.get('reason')
+            name = request.POST.get('name')
+            sclass = request.POST.get('class')
+            #return render(request, 'polls/c_approval.html',)
+            return HttpResponse("User is div : 2    ;" + str(amount) + str(reason)+ str(sclass) + str(name))
+        elif div ==s3:
+            return HttpResponse("Div is not equal to 1 but is equal to " + str(request.POST.get('div')))
+    else:
+        return render(request, 'polls/c_approval.html',)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################################################################
 
 
 def reg(request):
     context={}
     return render(request, 'polls/registar.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################################################################
+
 
 def hreg(request):
     context={}
@@ -46,27 +224,54 @@ def hreg(request):
     HttpResponse(html)
     return render(request,'polls/headregistar.html',context)
 
-def caddstud(request):
-    context={}
-    return render(request,'polls/c_addstudent.html',context)
+def haddstud(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        sclass = request.POST.get('class')
+        address = request.POST.get('add')
+        context={}
+        return render(request, 'polls/h_addstudent.html', context)
+    else:
+        return render(request, 'polls/h_addstudent.html',)
 
-def caddfac(request):
-    context1234={}
-    return render(request,'polls/c_addfaculty.html',context1234)
+def haddfac(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('add')
+        context={}
+        return render(request, 'polls/h_addfaculty.html', context)
+    else:
+        return render(request, 'polls/h_addfaculty.html', )
 
-def caddreg(request):
-    context={}
-    return render(request,'polls/c_addregistar.html',context)
+def haddstaff(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('add')
+        context={}
+        return render(request, 'polls/h_addstaff.html',context )
+    else:
+        return render(request, 'polls/h_addstaff.html',)
 
-def caddstaff(request):
-    context={}
-    return render(request,'polls/c_addstaff.html',context)
-
-def creq(request):
-    context45={}
-    return render(request,'polls/c_request.html',context45)
-
-
-def capp(request):
-    context46={}
-    return render(request,'polls/c_approval.html',context46)
+def hreq(request):
+    if request.method == 'POST':
+        div = request.POST.get('div')
+        s1="funds"
+        s2="concession"
+        s3="3"
+        if div == s1:
+            amount = request.POST.get('amount')
+            reason = request.POST.get('reason')
+            return render(request, 'polls/h_request.html',)
+        elif div == s2:
+            amount = request.POST.get('amount')
+            reason = request.POST.get('reason')
+            name = request.POST.get('name')
+            sclass = request.POST.get('class')
+            return render(request, 'polls/h_request.html',)
+        elif div ==s3:
+            return HttpResponse("Div is not equal to 1 but is equal to " + str(request.POST.get('div')))
+    else:
+        return render(request, 'polls/h_request.html',)
